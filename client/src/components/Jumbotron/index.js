4dd0d7481cca04b63ceb8e567c4f9a430a369c84
IDGB <49776938+ivergil@users.noreply.github.com>
2019-10-08 21:38:25 -0600

    create the Jumbotron component

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 function Jumbotron({ children }) {
+
+
   return (
     <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+      style={{ backgroundImage: `url(https://t3.ftcdn.net/jpg/01/38/57/38/240_F_138573824_W4VSqVz02lHfBwxvgkLJwcng1m9P3n7P.jpg)`,
+      backgroundSize: "cover", height: 600, clear: "both", paddingTop: 220, textAlign: "center" }}
       className="jumbotron"
     >
       {children}
