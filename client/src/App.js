2b6e476f129525616e100b37faf2a10dee854621
IDGB <49776938+ivergil@users.noreply.github.com>
2019-10-08 21:37:05 -0600

    created the grid component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import Saved from "./pages/saved";
+import Search from "./pages/search";
+import Detail from "./pages/detail";
+import NoMatch from "./pages/404"
 
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
+      
         <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
+          <Route exact path="/" component={Search} />
+          <Route exact path="/Search" component={Search} />
+          <Route exact path="/Saved" component={Saved} />
+          <Route exact path="/Book/:id" component={Detail}/> 
           <Route component={NoMatch} />
         </Switch>
       </div>
--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -1,12 +1,46 @@
 import React from "react";
 
 // Exporting the Container, Row, and Col components from this file
+const style={
+    borderStyle:{
+    borderStyle: "solid",
+    borderColor: "#17a2b8",
+    height:250,
+    paddingRight:20,
+    paddingLeft:20,
+    paddingTop:20,
+    marginBottom:20,
 
+    },
+    
+    borderStyleOne:{
+        borderStyle: "solid",
+        borderColor: "#17a2b8",
+        paddingRight:20,
+        paddingLeft:20,
+        paddingTop:20,
+        marginBottom:100,
+
+    
+        }
+
+}
 // This Container component allows us to use a bootstrap container without worrying about class names
 export function Container({ fluid, children }) {
   return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
 }
 
+export function Box ({children}){
+    return <div style={style.borderStyle}>
+    
+    {children}</div>
+}
+
+export function BoxOne ({children}){
+    return <div style={style.borderStyleOne}>
+    
+    {children}</div>
+}
 // This Row component lets us use a bootstrap row without having to think about class names
 export function Row({ fluid, children }) {
   return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
@@ -25,4 +59,4 @@ export function Col({ size, children }) {
       {children}
     </div>
   );
-}
+} 
