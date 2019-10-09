3dc68f0990281784f347b5163df3522443a0bcc0
IDGB <49776938+ivergil@users.noreply.github.com>
2019-10-08 21:45:11 -0600

    create the Result component

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,43 @@
 import React from "react";
+// import "../styles/navbar.css";
 
-function Nav() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
-  );
+
+const style={
+    navStyle:{
+    marginBottom:20,
+
+    }
 }
 
-export default Nav;
+function Navbar(props) {
+
+// return(<nav className="navbar"><ul><li style={{textShadow: props.shadow}} className="title navbar-brand"><a href="/">Clicky Game</a>
+// </li><li style={{color: props.color, textShadow: props.shadow}} className="navbar-text center">{props.message}</li><li style={{textShadow: props.shadow}} className="navbar-text score">Score: {props.score} | Top Score: {props.topScore}</li></ul></nav>);
+
+// }
+
+return(
+<nav style={style.navStyle} className="navbar navbar-expand-lg navbar-light bg-info">
+<a className=" text-light navbar-brand" href="/">Google Books Searcher</a>
+<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <span className="navbar-toggler-icon"></span>
+</button>
+<div className="collapse navbar-collapse" id="navbarNav">
+  <ul className="navbar-nav">
+    <li className= {props.search === true ? "nav-item" : 'nav-item active'}>
+      <a className="nav-link text-light" href="/Search">Search<span className="sr-only">(current)</span></a>
+    </li>
+    <li className= {props.saved === true ? "nav-item" : 'nav-item active'}>
+      <a className="nav-link text-light" href="/Saved">Saved</a>
+    </li>
+  </ul>
+</div>
+</nav>
+
+
+
+);
+
+}
+
+export default Navbar;
---
+++ client/src/components/resultsCard/index.js
@@ -0,0 +1,95 @@
+import React from "react";
+// import "../styles/resultsCard.css";
+
+//add props to display the cards dynamically....
+function Cards(props) {
+
+    const style = {
+        border: {
+            borderStyle: "solid",
+            borderColor: "lavender",
+            paddingRight: 20,
+            paddingLeft: 20,
+            paddingTop: 20,
+            paddingBottom: 20,
+            marginBottom: 20
+        },
+
+        div: {
+            float: "left",
+        },
+        divOne: {
+            float: "right"
+        },
+        pic: {
+            marginRight: 8
+        },
+        textsize: {
+            fontSize: 22,
+
+        },
+        textsizeAuthor: {
+            fontSize: 20,
+
+        }
+    }
+
+
+    let bookQuery = {
+        bookId: `${props.id}`,
+        title: `${props.bookTitle}`,
+        authors: `${props.authors}`,
+        description: `${props.description}`,
+        image: `${props.image}`,
+        link: `${props.link}`,
+    }
+
+
+
+
+    return (
+
+
+        <div style={style.border}>
+            <div className="row">
+
+                <div className="col-6">
+                    <div><h5 style={style.textsize} >{props.bookTitle}</h5></div>
+                    <div style={style.textsizeAuthor}>Written by: {props.authors}</div>
+                </div>
+
+                <div className="col-6 d-flex align-items-end flex-column">
+                    <div>
+                        <button className="btn btn-dark"><a href={`/Book/${props.id}`}> View </a> </button>
+                        <button className="btn btn-dark colorbtn" onClick={() => props.saveABook(bookQuery)} >Save</button>
+
+                    </div>
+                </div>
+
+
+            </div>
+
+
+
+
+            <div className="theContainer">
+
+                <div className="clear">
+                    <img style={style.pic} src={props.image} alt="book" />
+                </div>
+
+                <div className="clearfix">
+                    <div >{props.description}</div>
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+
+    );
+}
+
+export default Cards;
