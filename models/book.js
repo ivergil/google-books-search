27396be4f68033f79e9185de3dc46e761fd0541b
IDGB <49776938+ivergil@users.noreply.github.com>
2019-10-08 20:50:47 -0600

    Server routes and controllers

--- models/book.js
+++ models/book.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
+  bookId: { type: String },
+  title: { type: String, required: true, unique: true },
+  authors: { type: [String], required: true },
+  description: { String, unique: true },
+  image: String,
+  link: String, 
   date: { type: Date, default: Date.now }
 });
 
