13b56eed9525a9ecffcc50fdda6e15a1e488699c
IDGB <49776938+ivergil@users.noreply.github.com>
2019-10-08 21:50:34 -0600

    create the View component

---
+++ client/src/components/ViewCard/index.js
@@ -0,0 +1,101 @@
+import React from "react";
+// import "../styles/resultsCard.css";
+
+//add props to display the cards dynamically....
+function ViewCard(props, { children }) {
+
+    const style = {
+        border: {
+            borderStyle: "solid",
+            borderColor: "black",
+            paddingRight: 20,
+            paddingLeft: 20,
+            paddingTop: 20,
+            paddingBottom: 20,
+            marginBottom: 20
+        },
+
+        div: {
+            float: "left",
+        },
+        divOne: {
+            float: "right"
+        },
+        pic: {
+            marginRight: 8
+        },
+        textsize: {
+            fontSize: 22,
+
+        },
+        textsizeAuthor: {
+            fontSize: 20,
+
+        }
+    }
+
+
+    let bookQuery = {
+        bookId: `${props.id}`,
+        title: `${props.bookTitle}`,
+        authors: `${props.authors}`,
+        description: `${props.description}`,
+        image: `${props.image}`,
+        link: `${props.link}`,
+    }
+
+
+
+
+    return (
+
+
+        <div style={style.border}>
+            <div className="row">
+
+                <div className="col-6">
+                    <div><h5 style={style.textsize} >{props.bookTitle}</h5></div>
+                    <div style={style.textsizeAuthor}>Written by: {props.authors}</div>
+                </div>
+
+                <div className="col-6 d-flex align-items-end flex-column">
+                    <div>
+
+                        <button className="btn btn-dark colorbtn" onClick={() => props.saveABook(bookQuery)} >Save</button>
+
+                    </div>
+                </div>
+
+
+            </div>
+
+            <div className="theContainer">
+                <div>Pages: {props.pages}</div>
+                <div>Published by: {props.publisher}</div>
+                <div>Published on: {props.published}</div>
+
+            </div>
+
+
+
+            <div className="theContainer">
+
+                <div className="clear">
+                    <img style={style.pic} src={props.image} alt="book" />
+                </div>
+
+                <div className="clearfix">
+                    <div >{props.description}</div>
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+
+    );
+}
+
+export default ViewCard;
