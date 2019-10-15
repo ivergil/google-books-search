d4048d76d68d6e699056c3fadd531fd50da36fe6
IDGB <49776938+ivergil@users.noreply.github.com>
2019-10-14 19:24:42 -0600

    Adding /

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -17,6 +17,7 @@ module.exports = {
   },
   create: function(req, res) {
     db.Book
+    console.log(req.body)
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
